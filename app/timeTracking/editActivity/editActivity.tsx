71175012d23a61859ccbd8ac76f2b082cf589075
undefined1raven <zlat.dominic.francisc@gmail.com>
2025-06-09 22:39:24 +0300

    state save

--- app/timeTracking/editActivity/editActivity.tsx
+++ app/timeTracking/editActivity/editActivity.tsx
@@ -6,11 +6,47 @@ import { useGlobalStyleStore } from "@/stores/globalStyles";
 import FeatureConfigEmptySettingPage from "@/components/ui/FeatureConfigEmptySettingPage";
 import { router } from "expo-router";
 import { FeatureConfigValueInput } from "@/components/ui/FeatureConfigValueInput";
+import { useCallback } from "react";
+import { ARCTasksType } from "@/constants/CommonTypes";
+import { useFeatureConfigs } from "@/stores/featureConfigs";
+import { dataRetrivalApi } from "@/stores/dataRetriavalApi";
 function EditActivity() {
   const activityToEdit = useTimeTrackingSelectedActivity(
     (s) => s.activityToEdit
   );
   const globalStyle = useGlobalStyleStore((s) => s.globalStyle);
+
+  const updateAcitivty = useCallback(
+    (updatedActivity: ARCTasksType) => {
+      const featureConfigApi = useFeatureConfigs.getState();
+      const dataRetrivalAPI = dataRetrivalApi.getState();
+      if (activityToEdit === null) return;
+
+      dataRetrivalAPI
+        .modifyFeatureConfig(
+          "timeTracking",
+          ["itme", "taskID"],
+          updatedActivity.itme.taskID,
+          updatedActivity
+        )
+        .catch((error) => {
+          console.error("Failed to update activity:", error);
+        });
+
+      const updatedTimeTrackingFeatureConfig =
+        featureConfigApi.timeTrackingFeatureConfig.map((fcItem: any) => {
+          if (fcItem.itme.taskID === updatedActivity.itme.taskID) {
+            return updatedActivity;
+          }
+          return fcItem;
+        });
+      featureConfigApi.setTimeTrackingFeatureConfig(
+        updatedTimeTrackingFeatureConfig
+      );
+    },
+    [activityToEdit]
+  );
+
   return (
     <>
       <ThemedView style={{ ...styles.container, height: "100%" }}>
@@ -25,7 +61,17 @@ function EditActivity() {
           >
             <FeatureConfigValueInput
               inputType="text"
-              onChange={(newName: string) => {}}
+              onChange={(newName: string) => {
+                if (activityToEdit === null) return;
+                const updatedActivity: ARCTasksType = {
+                  ...activityToEdit,
+                  itme: {
+                    ...activityToEdit.itme,
+                    name: newName,
+                  },
+                };
+                updateAcitivty(updatedActivity);
+              }}
               label="Name"
               value={activityToEdit?.itme.name}
             ></FeatureConfigValueInput>
