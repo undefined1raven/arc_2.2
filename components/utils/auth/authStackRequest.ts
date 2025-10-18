6d51d17abbe449b2ff58f517f67f8efd052852b2
undefined1raven <zlat.dominic.francisc@gmail.com>
2025-10-18 11:06:29 +0300

    auth stack WIP

--- components/utils/auth/authStackRequest.ts
+++ components/utils/auth/authStackRequest.ts
@@ -2,8 +2,54 @@ import { API_URL } from "@/constants/API_URL";
 import { APP_ID } from "@/constants/app_id";
 import { useActiveUser } from "@/stores/activeUser";
 import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+import {
+  authChallengeStack,
+  secureStoreKeyNames,
+} from "../constants/secureStoreKeyNames";
+import { useCryptoOpsQueue } from "@/stores/cryptoOpsQueue";
+import { useActiveKeys } from "@/stores/decryptedKeys";
 
-function processAndSaveChallengeStack(challengeStack: string[]) {}
+async function processAndSaveChallengeStack(challengeStack: string[]) {
+  const existingStack = await SecureStore.getItemAsync(authChallengeStack);
+  let existingChallengeResponses: string[] = [];
+  if (existingStack) {
+    existingChallengeResponses = JSON.parse(existingStack);
+  }
+  const activePrivateKey = useActiveKeys.getState().activePrivateKey;
+  const decryptionPromises: any[] = [];
+
+  if (typeof activePrivateKey !== "string") {
+    return;
+  }
+
+  challengeStack.forEach((charCodeData) => {
+    const cryptoOps = useCryptoOpsQueue.getState();
+    const promise = cryptoOps.performOperation("decrypt", {
+      keyType: "private",
+      charCodeData: charCodeData,
+      decoding: "base64",
+      key: activePrivateKey,
+    });
+    decryptionPromises.push(promise);
+  });
+
+  const decryptedChallenges = await Promise.all(decryptionPromises);
+  const decryptedValues = decryptedChallenges.map(
+    (res) => res.payload?.decrypted
+  );
+  const isDecryptionSuccessful = decryptedValues.every(
+    (val) => typeof val === "string"
+  );
+
+  if (isDecryptionSuccessful === false) {
+    console.warn("Decryption of one or more challenges failed.");
+  }
+
+  const allValues = [...existingChallengeResponses, ...decryptedValues];
+
+  await SecureStore.setItemAsync(authChallengeStack, JSON.stringify(allValues));
+}
 
 function requestAuthChallengeStack() {
   const activeUserId = useActiveUser.getState().activeUser?.userId || null;
@@ -13,15 +59,20 @@ function requestAuthChallengeStack() {
     );
     return;
   }
-  console.log("Requesting auth challenge stack...");
   axios
     .post(`${API_URL}/auth/requestChallengeStack`, {
       data: { appID: APP_ID, accountID: activeUserId },
     })
     .then((response) => {
       const responseData = response.data;
-      console.log(responseData.challenges.length);
-      processAndSaveChallengeStack(responseData.challengeStack);
+      if (
+        responseData.status !== "success" ||
+        !Array.isArray(responseData.challenges)
+      ) {
+        console.warn("Failed to retrieve valid challenge stack:", responseData);
+        return;
+      }
+      processAndSaveChallengeStack(responseData.challenges);
     })
     .catch((error) => {
       console.log("Error requesting auth challenge stack:", error);
